80a2bf7c98ce220202355a2cf7c8e5b0c72b4570
Potoff <services.ankaamusic@gmail.com>
2023-01-10 19:34:24 +0100

    clean main page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { Menu } from '@headlessui/vue';
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <Menu>
+    </Menu>
   </main>
 </template>
