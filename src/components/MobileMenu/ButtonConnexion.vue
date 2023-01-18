35a1e1a38c13ab16312bfb2bde0372400771c492
Potoff <services.ankaamusic@gmail.com>
2023-01-18 18:15:35 +0100

    offcanva menu for mobile

---
+++ src/components/MobileMenu/ButtonConnexion.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="mt-6 mx-auto">
+      <button type="button" class="items-center rounded-md border w-60 border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-offset-1">
+        Connexion
+      </button>
+    </div>
+</template>
+
+<script setup>
+
+</script>
---
+++ src/components/MobileMenu/NavItemsMobile.vue
@@ -0,0 +1,38 @@
+<template>
+    <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95"
+        enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75"
+        leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
+        <MenuItems static
+            class="mt-1 w-48 focus:outline-none">
+            <MenuItem v-slot="{ active }">
+            <a href="#" 
+            class="mb-2 text-lg"
+            :class="[active ? 'bg-gray-100 rounded-lg' : '', 'block px-4 py-2 text-sm text-gray-700']">Potentiel
+                solaire</a>
+            </MenuItem>
+            <MenuItem v-slot="{ active }">
+            <a href="#" 
+            class="mb-2 text-lg"
+            :class="[active ? 'bg-gray-100 rounded-lg' : '', 'block px-4 py-2 text-sm text-gray-700']">Questions 
+                Réponses</a>
+            </MenuItem>
+            <MenuItem v-slot="{ active }">
+            <a href="#" 
+            class="mb-2 text-lg"
+            :class="[active ? 'bg-gray-100 rounded-lg' : '', 'block px-4 py-2 text-sm text-gray-700']">Installateurs
+                locaux</a>
+            </MenuItem>
+            <MenuItem v-slot="{ active }">
+            <a href="#" 
+            class="text-lg"
+            :class="[active ? 'bg-gray-100 rounded-lg' : '', 'block px-4 py-2 text-sm text-gray-700']">La carte
+                solaire</a>
+            </MenuItem>
+        </MenuItems>
+    </transition>
+</template>
+
+<script setup>
+import { MenuItem, MenuItems } from '@headlessui/vue';
+
+</script>
