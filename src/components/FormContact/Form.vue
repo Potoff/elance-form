73207e40e56c9d74dd1eb5806272d994f5f6ae39
Potoff <services.ankaamusic@gmail.com>
2023-02-03 12:56:10 +0100

    add finalForm

--- src/components/FormContact/Form.vue
+++ src/components/FormContact/Form.vue
@@ -11,7 +11,9 @@
                 </legend>
                 <div class="mt-10 w-60">
                     <div class="mb-3 rounded-xl shadow-lg bg-white">
-                        <input type="text" class="
+                        <input type="text"
+                            :class="[!name ? ['border-2', 'border-red-500', 'focus:border-red-600'] : ['border-2', 'border-green-500', 'focus:border-green-600']]"
+                            class="
         mx-auto
         form-control
         block
@@ -25,12 +27,13 @@
         rounded-xl
         transition
         ease-in-out
-        border-2 border-gray-200
         m-0
-      " id="exampleNumber0" placeholder="Votre nom" />
+      " id="exampleNumber0" placeholder="Votre nom" v-model="name" @input="checkInput" />
                     </div>
                     <div class="mb-3 rounded-xl shadow-lg bg-white">
-                        <input type="email" class="
+                        <input type="email"
+                            :class="[!mail ? ['border-2', 'border-red-500', 'focus:border-red-600'] : ['border-2', 'border-green-500', 'focus:border-green-600']]"
+                            class="
         mx-auto
         form-control
         block
@@ -44,10 +47,9 @@
         rounded-xl
         transition
         ease-in-out
-        border-2 border-gray-200
         m-0
         focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
-      " id="exampleNumber0" placeholder="Votre email" />
+      " id="exampleNumber0" placeholder="Votre email" v-model="mail" @input="checkInput" />
                     </div>
                     <div class="mb-3 rounded-xl shadow-lg bg-white">
                         <input type="text" class="
@@ -57,7 +59,7 @@
         w-full
         px-3
         py-1.5
-        text-base
+        text-xs
         font-normal
         text-gray-700
         bg-gray-50 
@@ -67,7 +69,7 @@
         border-2 border-gray-200
         m-0
         focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
-      " id="exampleNumber0" placeholder="Votre adresse postale" />
+      " id="exampleNumber0" placeholder="Votre adresse postale"  v-model="adress" />
                     </div>
                     <div class="flex mt-5 items-center">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -116,23 +118,24 @@
         border-2 border-gray-200
         m-0
         focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
-      " id="exampleNumber0" placeholder="Votre numéro de téléphone" />
+      " id="exampleNumber0" placeholder="Votre numéro de téléphone" v-bind="tel" />
                     </div>
                     <div class="text-center mt-10 text-red-500">
                         <p></p>
                     </div>
                     <div class="text-center mt-10">
-                        <button type="submit"
-                            class="inline-block px-6 py-2.5 bg-yellow-300 text-white font-medium text-xs leading-tight uppercase rounded-full ">Calcul
-                            de la production d'électricité
+                        <button type="button"
+                            class="inline-block px-6 py-2.5 bg-yellow-300 text-white font-medium text-xs leading-tight uppercase rounded-full "
+                            :class="[!btnDisabled ? ['bg-yellow-500', 'shadow-md', 'hover:bg-yellow-600', 'hover:shadow-lg', 'focus:bg-yellow-600', 'focus:shadow-lg', 'focus:outline-none', 'focus:ring-0', 'active:bg-yellow-700', 'active:shadow-lg', 'transition', 'duration-150', 'ease-in-out'] : '']"
+                            :disabled="btnDisabled">Calcul de la production d'électricité
                         </button>
 
                     </div>
                 </div>
             </form>
         </div>
-            <Map>
-            </Map>
+        <Map @click="getCoordinate">
+        </Map>
     </div>
 </template>
 
@@ -145,6 +148,28 @@ import Map from './Map.vue';
 const enabled = ref(false)
 const showNumber = ref(true)
 const phoneMessage = ref('je souhaite être rappelé')
+const adress = ref('')
+
+const name = ref('')
+const mail = ref('')
+const tel = ref(0)
+const btnDisabled = ref(true)
+
+
+function getCoordinate(e) {
+    adress.value = e
+}
+
+
+function checkInput(e) {
+    if (mail.value && name.value && adress.value) {
+        btnDisabled.value = false
+        return true
+    } else {
+        btnDisabled.value = true
+        return false
+    }
+};
 
 
 let showTel = () => {
--- src/components/FormContact/Map.vue
+++ src/components/FormContact/Map.vue
@@ -1,22 +1,78 @@
 <template>
     <ol-map :loadTilesWhileAnimating="true" :loadTilesWhileInteracting="true" style="height:400px"
-        class="w-full mt-10 xl:mt-0"
-        @click="getCoordinate"
-        >
+        class="w-full mt-10 xl:mt-0" @click="getCoordinate">
 
         <ol-view ref="view" :center="center" :rotation="rotation" :zoom="zoom" :projection="projection" />
 
         <ol-tile-layer>
             <ol-source-osm />
         </ol-tile-layer>
+
+        <ol-vector-layer>
+            <ol-source-vector ref="vectors">
+                <ol-interaction-draw @drawstart="drawstart" :type="drawType">
+                </ol-interaction-draw>
+            </ol-source-vector>
+
+            <ol-style>
+                <ol-style-icon :src="markerIcon" :scale="0.05"></ol-style-icon>
+            </ol-style>
+        </ol-vector-layer>
+
+
     </ol-map>
 </template>
 
+
+
 <script setup>
-function getCoordinate(e){
-    e.stopPropagation();
-    const coordinates = e.frameState
-    console.log(coordinates)
+import { ref } from "vue";
+import markerIcon from "../icons/Marker.svg"
+
+const center = ref([2, 46.7])
+const projection = ref('EPSG:4326')
+const zoom = ref(6)
+const rotation = ref(0)
+const adress = ref('')
+
+const markers = ref(null);
+const drawType = ref("Point")
+
+const drawedMarker = ref()
+const vectors = ref(null);
+
+const drawstart = (event) => {
+    vectors.value.source.removeFeature(drawedMarker.value);
+    drawedMarker.value = event.feature;
+    console.log(vectors.value.source)
 }
 
+let emit = defineEmits(['click'])
+
+function getCoordinate(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    let lat = e.target.pixelToCoordinateTransform_[5].toFixed(4)
+    let long = e.target.pixelToCoordinateTransform_[4].toFixed(4)
+    let api_key = 'ab6d70467aed4746bb66cf3c7fcb3717'
+    fetch(`https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${long}&format=json&apiKey=${api_key}`)
+        .then((res) => res.json())
+        .then((json) => {
+            adress.value = json.results[0].formatted
+            emit('click', adress.value)
+        })
+}
+
+
+let api_key = 'ab6d70467aed4746bb66cf3c7fcb3717'
+
 </script>
+
+<style>
+.overlay-content {
+    background: #efefef;
+    box-shadow: 0 5px 10px rgb(2 2 2 / 20%);
+    padding: 10px 20px;
+    font-size: 16px;
+}
+</style>
